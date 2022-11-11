6feaa0034b949b42b3e8607f8520422c7a996245
Vadim <skinnoniks@gmail.com>
2022-11-11 13:13:33 +0300

    Added more helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,12 +1,33 @@
 <?php
 
+use SkinonikS\Laravel\Modules\Module\Module;
 use SkinonikS\Laravel\Modules\Repository\Repository;
+
+if (! function_exists('modules')) {
+    function modules(): Repository
+    {
+        return app(Repository::class);
+    }
+}
+
+if (! function_exists('module')) {
+    function module(string $moduleId): ?Module
+    {
+        return modules()->find($moduleId);
+    }
+}
+
+if (! function_exists('moduleOrFail')) {
+    function moduleOrFail(string $moduleId): Module
+    {
+        return modules()->findOrFail($moduleId);
+    }
+}
 
 if (! function_exists('module_path')) {
     function module_path(string $moduleId, string $path = ''): string
     {
-        return app(Repository::class)
-            ->findOrFail($moduleId)
+        return moduleOrFail($moduleId)
             ->modulePath($path);
     }
 }
